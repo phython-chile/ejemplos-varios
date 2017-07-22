5909d76db009f5e89a0220f34da6f11eecbcd2bf
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-07-22 01:05:03 -0400

    Numeros Enteros

---
+++ saber_numero_es_entero.py
@@ -0,0 +1,14 @@
+#create a float number
+f = 100.0
+print(f)
+
+#this method verify if the float instance is finite with integral value.
+#Return True or False.
+print(f.is_integer())
+
+#create a float number
+f = 46.12
+print(f)
+
+#return True because the decimal part is 0
+print(f.is_integer())
